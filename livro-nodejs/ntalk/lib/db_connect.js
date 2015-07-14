20019a96d9954f8140856e361a94a3e5e5bab705
Paulo Henrique <paulohenrique@eggtech.com.br>
2015-07-14 13:04:37 -0300

    Capítulo 10.3 - Aplicando Singleton nas conexões do Mongoose

--- livro-nodejs/ntalk/lib/db_connect.js
+++ livro-nodejs/ntalk/lib/db_connect.js
@@ -1,8 +1,12 @@
+var mongoose 			= require('mongoose'),
+	config				= require('../config'),
+	env					= process.env.NODE_ENV || 'development',
+	url 				= config.MONGODB[env],
+	single_connection;
+		
 module.exports = function(){
-	var mongoose 	= require('mongoose'),
-		config		= require('../config'),
-		env			= process.env.NODE_ENV || 'development',
-		url 		= config.MONGODB[env];
-	
-	return mongoose.connect(url);
+	if(!single_connection)
+		single_connection = mongoose.connect(url);
+		
+	return single_connection;
 };
