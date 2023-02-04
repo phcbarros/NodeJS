09c7f26b92eab8979039abc4f4c5d07dea34548f
phcbarros <paulo.barros@outlook.com.br>
2023-02-04 13:36:13 -0300

    Error Handler: Docker - Encerre com segurança os aplicativos da web Node.js

--- error-handler/src/index.ts
+++ error-handler/src/index.ts
@@ -8,3 +8,8 @@ const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
+
+function handle(signal: unknown) {
+  console.log(`*^!@4=> Received event: ${signal}`)
+}
+process.on('SIGHUP', handle)
